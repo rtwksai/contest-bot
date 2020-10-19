6d6b19fff44392021a1d09adeb295fce9a25dd3d
DaKeiser <rithwiksaim@gmail.com>
2020-10-20 01:02:09 +0530

    Command to send contest query

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import discord
 from discord.ext import commands
-from utils import cf
+from utils import cf, utility
+from datetime import datetime
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -20,11 +21,27 @@ class Commands(commands.Cog):
         Usage: -cfnext 
                -cfnext [count]
         '''
-        url = "https://codeforces.com/api/contest.list"
-        c = cf.Codeforces(url)
+        api_url = "https://codeforces.com/api/contest.list"
+        c = cf.Codeforces(api_url)
         result = c.getContests()
-        for contest in result[:len(result)-count-1:-1]:
-            await ctx.author.send(f"{contest['name']}")
+
+        title="Codeforces"
+        url="https://codeforces.com/contests"
+        description="List of upcoming contests on Codeforces"
+        t_url="https://sta.codeforces.com/s/78793/favicon-32x32.png"
+
+        a = utility.Embeds(title, url, description, t_url)
+
+        #Change this algo, sort according to the nearing time
+        #Currently, it just takes the bottom three contests
+        for contest,i in zip(result[:len(result)-count-1:-1],range(count)):
+            name = "Contest"+str(i+1)
+            value = contest['name']
+            date = datetime.fromtimestamp(contest['startTimeSeconds'])
+            a.create_embed(name, value, date)
+            x = a.get_embed()
+
+        await ctx.author.send(embed=x)
 
 def setup(bot):
     bot.add_cog(Commands(bot))
