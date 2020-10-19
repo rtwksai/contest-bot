787b552b89190755848dafd53f505bc084a9b8a3
DaKeiser <rithwiksaim@gmail.com>
2020-10-19 15:59:47 +0530

    command to show the nearest contests

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import discord
 from discord.ext import commands
+from utils import cf
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -8,11 +9,22 @@ class Commands(commands.Cog):
     
     @commands.command()
     async def ping(self, ctx):
-        '''
-        This command returns the ping.
+        '''This command returns the ping.
         Usage: -ping
         '''
         await ctx.send(f'Pong! {round(self.bot.latency * 1000)} ms')
+
+    @commands.command(name = 'cfnext')
+    async def next_codeforces(self, ctx, count=3):
+        '''Messages 3 nearest contests on CF
+        Usage: -cfnext 
+               -cfnext [count]
+        '''
+        url = "https://codeforces.com/api/contest.list"
+        c = cf.Codeforces(url)
+        result = c.getContests()
+        for contest in result[:len(result)-count-1:-1]:
+            await ctx.author.send(f"{contest['name']}")
 
 def setup(bot):
     bot.add_cog(Commands(bot))
