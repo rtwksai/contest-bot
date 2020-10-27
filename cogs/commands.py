268151eb43a7c7260e80471cfe28b9d05f32ce54
NikiAdivi <geenanik25@gmail.com>
2020-10-27 18:44:01 +0530

    Tried using reactions for reminders

--- cogs/commands.py
+++ cogs/commands.py
@@ -13,6 +13,9 @@ class Commands(commands.Cog):
         self.title="Codeforces"
         self.url="https://codeforces.com/contests"
         self.t_url="https://sta.codeforces.com/s/78793/favicon-32x32.png"
+        self.left = '⏪'
+        self.right = '⏩'
+        self.remind_3 = '3️⃣'
     
     @commands.command()
     async def ping(self, ctx):
@@ -20,6 +23,19 @@ class Commands(commands.Cog):
         Usage: -ping
         '''
         await ctx.send(f'Pong! {round(self.bot.latency * 1000)} ms')
+
+    def predicate(self, message, l, r):
+        def check(reaction, user):
+            if reaction.message.id != message.id or user == self.bot.user:
+                return False
+            if l and reaction.emoji == self.left:
+                return True
+            if r and reaction.emoji == self.right:
+                return True
+            if reaction.emoji == self.remind_3:
+                return True
+            return False
+        return check
 
     @commands.command(name = 'cfnext')
     async def next_codeforces(self, ctx, count=3):
@@ -31,16 +47,61 @@ class Commands(commands.Cog):
         result = c.get_contests()
         description="List of upcoming contests on Codeforces"
 
-        a = utility.Embeds(self.title, self.url, description, self.t_url)
-
-        for contest in result[:count]:
+        messages = []
+        contest_objects = result[:count]
+        for contest in contest_objects:
+            a = utility.Embeds(self.title, self.url, description, self.t_url)
             name = "Contest: "+str(contest['id'])
             value = contest['name']
             date = datetime.fromtimestamp(contest['startTimeSeconds'])
             a.create_embed(name, value, date)
-            embed = a.get_embed()
+            messages.append(a.get_embed())
 
-        await ctx.author.send(embed=embed)
+        # await ctx.author.send(embed=embeds[1])
+
+        index = 0
+        msg = None
+        action = ctx.author.send
+        while True:
+            res = await action(embed=messages[index])
+
+            if res is not None:
+                msg = res 
+            l = index != 0
+            r = index != len(messages) - 1
+
+            await msg.add_reaction(self.left) 
+            await msg.add_reaction(self.right)
+            await msg.add_reaction(self.remind_3)
+
+            react, user = await self.bot.wait_for('reaction_add', check=self.predicate(msg, l, r))
+            
+            if react.emoji == self.left:
+                index -= 1
+
+            elif react.emoji == self.right:
+                index += 1
+
+            elif react.emoji == self.remind_3:
+                contest = contest_objects[index]
+                when_to_remind = 135*60
+                print("Hello")
+                await asyncio.sleep(contest['relativeTimeSeconds']*(-1) - when_to_remind)
+                print("After")
+                title="Codeforces Contest Reminder"
+                description="Contest begins in {0}".format(time.strftime("%H hours, %M minutes, %S seconds",
+                                                            time.gmtime(abs(contest['relativeTimeSeconds']))))
+                a = utility.Embeds(title, self.url, description, self.t_url, 0xF93A2F)
+
+                name = "Contest: "+str(contest['id'])
+                value = contest['name']
+                date = datetime.fromtimestamp(contest['startTimeSeconds'])
+                a.create_embed(name, value, date)
+                embed = a.get_embed()
+
+                await ctx.author.send(embed=embed)
+
+            action = msg.edit
 
     @commands.command(name = 'notify')
     async def notify_contest(self, ctx):
